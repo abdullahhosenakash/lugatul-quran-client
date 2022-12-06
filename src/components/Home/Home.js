334d8d20dfd7c6a03da209ac5c2c08d462165d93
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-12-06 08:19:11 +0600

    search error added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import { Form, Table } from "react-bootstrap";
-import useWords from "../../hooks/useWords";
-import "../Shared/Shared.css";
+import React, { useState } from 'react';
+import { Form, Table } from 'react-bootstrap';
+import useWords from '../../hooks/useWords';
+import '../Shared/Shared.css';
 
 const Home = () => {
-  const [searchedText, setSearchedText] = useState("");
-  const [words] = useWords(searchedText);
+  const [searchedText, setSearchedText] = useState('');
+  const [words, loading] = useWords(searchedText);
 
   return (
     <div className="container mt-5 pt-4 home-page">
@@ -15,7 +15,8 @@ const Home = () => {
       >
         <Form.Control
           type="text"
-          className="text-end fs-1 input-field-height"
+          className={`text-end input-field-height 
+          ${searchedText ? 'input-field-font-size' : 'fs-1'}`}
           placeholder="আরবী শব্দ খুঁজুন"
           onChange={(e) => setSearchedText(e.target.value)}
         />
@@ -29,12 +30,32 @@ const Home = () => {
           </tr>
         </thead>
         <tbody>
-          {words.map((word, index) => (
-            <tr key={index}>
-              <td className="fs-5">{word.banglaMeaning}</td>
-              <td className="fs-1">{word.arabicWord}</td>
-            </tr>
-          ))}
+          {words.length ? (
+            words.map((word, index) => (
+              <tr key={index}>
+                <td className="fs-5 align-middle">{word.banglaMeaning}</td>
+                <td className="arabic-word-result align-middle">
+                  {word.arabicWord}
+                </td>
+              </tr>
+            ))
+          ) : searchedText ? (
+            loading ? (
+              <tr>
+                <td colSpan={2} className="text-success fs-4">
+                  শব্দটি খোজা হচ্ছে
+                </td>
+              </tr>
+            ) : (
+              <tr>
+                <td colSpan={2} className="text-danger fs-4">
+                  দুঃখিত! শব্দটি পাওয়া যায়নি অথবা এখনও ডাটাবেজে যোগ করা হয়নি
+                </td>
+              </tr>
+            )
+          ) : (
+            <tr></tr>
+          )}
         </tbody>
       </Table>
     </div>
--- src/hooks/useWords.js
+++ src/hooks/useWords.js
@@ -3,17 +3,22 @@ import { useState } from "react";
 
 const useWords = (searchedText) => {
   const [words, setWords] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    setLoading(true);
     if (searchedText) {
       fetch(`https://lugatul-quran-server.onrender.com/getWord/${searchedText}`)
         .then((res) => res.json())
-        .then((data) => setWords(data.result));
+        .then((data) => {
+          setWords(data.result);
+          setLoading(false);
+        });
     } else {
       setWords([]);
     }
   }, [searchedText]);
 
-  return [words];
+  return [words, loading];
 };
 
 export default useWords;
